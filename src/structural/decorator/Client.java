efae0690e40677fcea02e7bed4637fb69f3688ba
wendy3 <2511347599@qq.com>
2019-07-17 23:58:38 +0800

    1、适配器模式 2、装饰器模式

--- src/structural/decorator/Client.java
+++ src/structural/decorator/Client.java
@@ -1,4 +1,9 @@
 package structural.decorator;
 
 public class Client {
+    public static void main(String[] args) {
+        ConcreteComponent component = new ConcreteComponent();
+        ConcreteDecorator decorator = new ConcreteDecorator(component);
+        decorator.sampleOperation();
+    }
 }
