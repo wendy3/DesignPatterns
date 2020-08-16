4f2c3a3fc4747f455959e248af7815504b3988b2
wendy3 <2511347599@qq.com>
2020-08-16 16:17:38 +0800

    桥接模式学习

---
+++ src/structural/bridge/RefinedAbstractionA.java
@@ -0,0 +1,12 @@
+package structural.bridge;
+
+/**
+ * @author gww
+ * @date 2020/08/16
+ */
+public class RefinedAbstraction extends Abstraction {
+    @Override
+    public void operation() {
+        
+    }
+}
---
+++ src/structural/bridge/RefinedAbstractionB.java
@@ -0,0 +1,8 @@
+package structural.bridge;
+
+/**
+ * @author gww
+ * @date 2020/08/16
+ */
+public class RefinedAbstractionB {
+}
